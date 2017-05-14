0ca3a8ecbacdc6a44ac104a3628d5f67516e37bd
Brett East <brett@sproutback.com>
2017-05-14 16:47:11 +1000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  //socket.emit message from admin 'Welcome to the chat app'
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  //socket.broadcast.emit from admin 'New user joined'
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('disconnect', function() {
     console.log('Client disconnected');
   });
@@ -24,7 +37,12 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 
